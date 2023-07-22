b0d32afac6a408b2996b03eb24230aeafb9a082c
이준영 <joonyeong0126@gmail.com>
2023-07-23 01:04:05 +0900

    struct

--- library.c
+++ library.c
@@ -6,6 +6,30 @@ void graph() {
 
 }
 
+void tree() {
+
+}
+
+void deque() {
+
+}
+
+void queue() {
+
+}
+
+void stack() {
+
+}
+
+void list() {
+
+}
+
+void node() {
+
+}
+
 void hello(void) {
     printf("Hello, World!\n");
 }
