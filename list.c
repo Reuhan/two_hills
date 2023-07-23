d13c9cbf825d22491b39622b8bf22a226b68642c
이준영 <joonyeong0126@gmail.com>
2023-07-23 20:55:59 +0900

    list complete?

--- list.c
+++ list.c
@@ -3,29 +3,86 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void insert(int32_t data, Node *n) {
-    Node new;
-    new.data = data;
-    new.next = NULL;
+node *head = NULL;
+node *tail = NULL;
 
-    while (n->next != NULL) {
-        n = n->next;
-    }
-    n->next = &new;
+/**
+ * 생성자
+ */
+list constructor() {
+    node *new = malloc(sizeof(node));
+    new->data = 0;
+    new->next = NULL;
+
+    head = new;
+    tail = new;
+
+    return new;
 }
 
+/**
+ * 소멸자
+ */
+void destructor(list ls) {
+    free(ls);
+}
 
-bool delete(int32_t data, Node *n) {
-    if (search(data, *n)) {
-        while (n->next->data != data) {
-            n = n->next;
+void insertHead(int32_t data, list ls) {
+    list new = malloc(sizeof(node));
+    new->data = data;
+    new->next = ls;
+
+    head = new;
+}
+
+void insertTail(int32_t data, list ls) {
+    list new = malloc(sizeof(node));
+    new->data = data;
+    new->next = NULL;
+
+    while (ls->next != NULL) {
+        ls = ls->next;
+    }
+
+    ls->next = new;
+
+    tail = new;
+}
+
+/**
+ * 삭제
+ * @param data 삭제할 값
+ * @param ls 삭제할 리스트
+ */
+void delete(int32_t data, list ls) {
+    node *del = ls;
+    if (search(data, *del)) {
+        while (del->data != data) {
+            ls = del;
+            del = del->next;
         }
-        n->next = n->next->next;
+
+        if (del == head) {
+            ls = ls->next;
+            del->next = NULL;
+            head = ls;
+        } else if (del == tail) {
+            ls->next = NULL;
+            tail = ls;
+        } else {
+            ls->next = ls->next->next;
+        }
+        destructor(del);
     }
-    return false;
 }
 
-bool search(int32_t data, Node node) {
+/**
+ * 찾고 싶은 값이 있는지 확인한다.
+ * @param data 검색할 데이터
+ * @param node 데이터를 찾을 리스트
+ * @return 검색할 요소가 있으면 true 없으면 false
+ */
+bool search(int32_t data, node node) {
     while (node.next != NULL) {
         if (node.data == data) {
             return true;
@@ -35,9 +92,22 @@ bool search(int32_t data, Node node) {
     return false;
 }
 
-void view(Node list) {
-    while (list.next != NULL) {
-        printf("%d ", list.data);
-        list = *list.next;
-    }
+/**
+ * 출력
+ * @param ls 출력할 리스트
+ */
+void view(list ls) {
+    do {
+        printf("%d ", ls->data);
+        ls = ls->next;
+    } while (ls != NULL);
+    printf("\n");
+}
+
+void Head() {
+    printf("%d\n", head->data);
+}
+
+void Tail() {
+    printf("%d\n", tail->data);
 }
--- node.h
+++ node.h
@@ -1,18 +1,30 @@
 #ifndef TWO_HILLS_NODE_H
 #define TWO_HILLS_NODE_H
+
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct node Node;
+typedef struct node node;
+typedef struct node *list;
+typedef struct node *stack;
 
 struct node {
     int32_t data;
     struct node *next;
 };
 
-void insert(int32_t, Node*);
-bool delete(int32_t, Node*);
-bool search(int32_t, Node);
-void view(Node);
+list constructor();
+
+void destructor(list);
+
+void insertHead(int32_t data, list ls);
+
+void insertTail(int32_t data, list ls);
+
+void delete(int32_t, node *);
+
+bool search(int32_t, node);
+
+void view(node *);
 
 #endif //TWO_HILLS_NODE_H
