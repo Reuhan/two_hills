cfeede39a8c827cba9fb190a63e119d3c34f1866
이준영 <joonyeong0126@gmail.com>
2023-07-25 19:05:46 +0900

    list complete

--- list.c
+++ list.c
@@ -7,18 +7,22 @@ struct List *init() {
     struct List *new = malloc(sizeof(struct List));
     new->first = NULL;
     new->last = NULL;
+
     return new;
 }
 
 void firstInsert(int data, struct List *ls) {
-    struct Node *temp = ls->first;
-    ls->first = malloc(sizeof(struct Node));
+    if (ls->first == NULL) {
+        ls->first = malloc(sizeof(struct Node));
 
-    ls->first->data = data;
-    ls->first->next = temp;
-
-    if (ls->last == NULL) {
+        ls->first->data = data;
         ls->last = ls->first;
+    } else {
+        struct Node *temp = ls->first;
+        ls->first = malloc(sizeof(struct Node));
+
+        ls->first->data = data;
+        ls->first->next = temp;
     }
 }
 
@@ -29,11 +33,18 @@ void firstDelete(struct List *ls) {
 }
 
 void lastInsert(int data, struct List *ls) {
-    struct Node *temp = ls->last;
-    ls->last = malloc(sizeof(struct Node));
+    if (ls->last == NULL) {
+        ls->last = malloc(sizeof(struct Node));
 
-    ls->last->data = data;
-    temp->next = ls->last;
+        ls->last->data = data;
+        ls->first = ls->last;
+    } else {
+        struct Node *temp = ls->last;
+        ls->last = malloc(sizeof(struct Node));
+
+        ls->last->data = data;
+        temp->next = ls->last;
+    }
 }
 
 void view(struct List *ls) {
@@ -41,5 +52,6 @@ void view(struct List *ls) {
     do {
         printf("%d ", iter->data);
         iter = iter->next;
-    } while (iter->next != NULL);
+    } while (iter != NULL);
+    printf("\n");
 }
